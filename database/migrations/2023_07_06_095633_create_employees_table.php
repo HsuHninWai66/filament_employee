e9acf27ab2cb1c6f82eeba9a4b927a8d7a5273b6
Neroaqucious <hsuhninwai66@gmail.com>
2023-07-06 16:38:07 +0630

    first

---
+++ database/migrations/2023_07_06_095633_create_employees_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('country_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('state_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('city_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('department_id')->constrained()->cascadeOnDelete();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('gender')->nullable();
+            $table->string('birthdate');
+            $table->string('phone_number');
+            $table->string('email');
+            $table->string('address')->nullable();
+            $table->string('role');
+            $table->string('office_time');
+            $table->string('em_start_date');
+            $table->string('experience_yr'); // Calculate current date with em_start_date
+            $table->string('profile_img')->nullable();
+            $table->string('sign')->nullable();
+            $table->string('remark')->nullable();
+            $table->string('kbz_bank_acc')->nullable();
+            $table->string('kbz_pay')->nullable();
+            $table->string('aya_bank')->nullable();
+            $table->string('yoma_bank')->nullable();
+            $table->string('wave_money_number')->nullable();
+            $table->string('status');  // Still working or Resigned form work
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
